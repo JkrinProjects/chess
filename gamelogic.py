a900e800baaae558828f8707642bb46bc464aafa
jk393 <jkcodingprojects@proton.me>
2026-09-01 17:44:26 -0400

    added input validation, moved board to grid conversion to seperate func

--- gamelogic.py
+++ gamelogic.py
@@ -14,22 +14,52 @@ def column_letter_to_number(letter):
     base = ord("a")
     return(ord(letter)-base)
 
+#function to validate input and convert chess notation to board.grid location (square A6 -> position (6,0))
+def convert_chess_square_to_grid(square):
+    if len(square) != 2:
+        return ValueError("That is not a valid square")
+
+    board_square = list(square)
+    input_column = board_square[0].lower()
+    input_row = board_square[1]
+
+    if input_column < "a" or input_column > "h":
+        return ValueError("Invalid Column")
+
+    if input_row < "1" or input_row > "8":
+        return ValueError("Invalid Row")
+
+    grid_column = column_letter_to_number(input_column)
+    grid_row = BOARD_LENGTH - int(input_row)
+
+    return grid_row, grid_column
+
+
+
 def move(board, starting_square_name, ending_square_name):
 
     #create a list of the square name to parse the col/row
-    starting_board_square = list(starting_square_name)
-    ending_board_square = list(ending_square_name)
+    #starting_board_square = list(starting_square_name)
+    #ending_board_square = list(ending_square_name)
 
-    #map the square name to the table position, square A6 -> position (0,2)
-    starting_row = BOARD_LENGTH - int(starting_board_square[1])
-    starting_col = column_letter_to_number(starting_board_square[0])
+    #map the square name to the table position
+    starting_row, starting_col = convert_chess_square_to_grid(starting_square_name)
+    ending_row, ending_col = convert_chess_square_to_grid(ending_square_name)
+    
+    
+    #get the piece at the grid location
     start_square_piece = board.grid[starting_row][starting_col] #the piece in this positin
-
-    ending_row = BOARD_LENGTH - int(ending_square_name[1]) 
-    ending_col = column_letter_to_number(ending_board_square[0])
     end_square_piece = board.grid[ending_row][ending_col] #should be empty, can use its emptiness to verify valid move
 
 
     #Move the piece
     board.grid[starting_row][starting_col] = end_square_piece
     board.grid[ending_row][ending_col] = start_square_piece
+
+    ###Older code
+    #starting_row = BOARD_LENGTH - int(starting_board_square[1])
+    #starting_col = column_letter_to_number(starting_board_square[0])
+    
+
+    #ending_row = BOARD_LENGTH - int(ending_square_name[1]) 
+    #ending_col = column_letter_to_number(ending_board_square[0])
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def main():
     game_board = Board()
     game_board.print_board()
 
-    
-    start_position = input("Starting Position: ")
-    end_position = input("Ending Position: ")
-    move(game_board, start_position, end_position)
-    game_board.print_board()
+    while True:
+        start_position = input("Starting Position: ")
+        end_position = input("Ending Position: ")
+        move(game_board, start_position, end_position)
+        game_board.print_board()
 
     
 if __name__ == "__main__":
