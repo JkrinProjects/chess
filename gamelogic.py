5b876a808f93880147cbf889731051bd26752351
jk393 <jkcodingprojects@proton.me>
2026-09-06 20:52:43 -0400

    edited move() function to match input expected from Pieces. added calculation for row/col difference of a move

--- gamelogic.py
+++ gamelogic.py
@@ -19,17 +19,17 @@ def column_letter_to_number(letter):
 #function to validate input and convert chess notation to board.grid location (square A6 -> position (6,0))
 def convert_chess_square_to_grid(square):
     if len(square) != 2:
-        return ValueError("That is not a valid square")
+        raise ValueError("That is not a valid square")
 
     board_square = list(square)
     input_column = board_square[0].lower()
     input_row = board_square[1]
 
     if input_column < "a" or input_column > "h":
-        return ValueError("Invalid Column")
+        raise ValueError("Invalid Column")
 
     if input_row < "1" or input_row > "8":
-        return ValueError("Invalid Row")
+        raise ValueError("Invalid Row")
 
     grid_column = column_letter_to_number(input_column)
     grid_row = BOARD_LENGTH - int(input_row)
@@ -43,19 +43,22 @@ def move(board, starting_square_name, ending_square_name):
     #map the square name to the table position
     starting_row, starting_col = convert_chess_square_to_grid(starting_square_name)
     ending_row, ending_col = convert_chess_square_to_grid(ending_square_name)
-    
+
+    row_difference = ending_row - starting_row
+    column_difference = ending_col - starting_col
     
     #get the piece at the grid location
-    start_square_piece: Piece = board.grid[starting_row][starting_col] #the piece in this positin
-    end_square_piece: Piece = board.grid[ending_row][ending_col] #should be empty or a capturable piece, can use its emptiness to verify valid move
+    moving_piece: Piece = board.grid[starting_row][starting_col] #the piece in this positin
+    destination_of_moving_piece: Piece = board.grid[ending_row][ending_col] #should be empty or a capturable piece, can use its emptiness to verify valid move
 
     #check if the movement is valid
-    if start_square_piece.is_valid_piece_movement()
+    if moving_piece.is_valid_piece_movement(row_difference, column_difference):
+        #Move the piece
+        board.grid[starting_row][starting_col] = destination_of_moving_piece
+        board.grid[ending_row][ending_col] = moving_piece
 
-
-    #Move the piece
-    board.grid[starting_row][starting_col] = end_square_piece
-    board.grid[ending_row][ending_col] = start_square_piece
+        return True
+    return False
 
     ###Older code
     #starting_row = BOARD_LENGTH - int(starting_board_square[1])
