26e7de59e38a8c93d1271e1ff1ff253b265f8680
jk393 <jkcodingprojects@proton.me>
2026-09-06 20:50:05 -0400

    moved calculatin of row/col differnce to gamelogic. Simplified if statements, added rook and queen movement

--- pieces.py
+++ pieces.py
@@ -1,10 +1,4 @@
 from abc import ABC, abstractmethod
-from gamelogic import convert_chess_square_to_grid
-
-
-
-
-
 
 
 class Piece(ABC):
@@ -12,23 +6,12 @@ class Piece(ABC):
         self.color = color
         self.letter = None
 
-    def get_row_and_column_difference(self, starting_square, ending_square):
-        #get table coordinates from chess notation
-        starting_row, starting_col = convert_chess_square_to_grid(starting_square)
-        ending_row, ending_col = convert_chess_square_to_grid(ending_square)
-
-        #find difference
-        row_difference = ending_row - starting_row
-        column_difference = ending_col - starting_col
-
-        return row_difference, column_difference
-
     #print function
     def __str__(self):
         return f"{self.color}: {self.letter}"
     
     @abstractmethod
-    def is_valid_piece_movement(self, starting_square, ending_square):
+    def is_valid_piece_movement(self, row_difference, column_difference):
         pass
 
 class Pawn(Piece):
@@ -37,44 +20,40 @@ class Pawn(Piece):
         super().__init__(color)
         self.letter = "P"
 
-    def is_valid_piece_movement(self, starting_square, ending_square):
-
-        row_difference, column_difference = self.get_row_and_column_difference(starting_square, ending_square)
+    def is_valid_piece_movement(self, row_difference, column_difference):
 
         #black pawns only move +1 row, white pawns only move -1 row
 
         #black pawn moveme
         if self.color == "black":
-            if(column_difference == 0 and row_difference == 1)
+
+            #move down the board
+            if(column_difference == 0 and row_difference == 1):
                 return True
-            #capture
+            #capture down
             if ((column_difference == 1 or column_difference == -1) and row_difference == 1):
                 return True
-        return False
             
-                
         #white pawn movement
-        if self.color == "white":
-            if column_difference == 0:
-                if row_difference == -1:
-                    return True
-                else:
-                    return False
-
-        #capture
-        if column_difference == 1 or column_difference == -1:
-            if row_difference == 1:
+        if self.color == "white":   
+            #move up the board
+            if(column_difference == 0 and row_difference == -1):
                 return True
-            else:
-                return False
-
+            #capture up
+            if ((column_difference == 1 or column_difference == -1) and row_difference == -1):
+                return True
+        return False
 
     
 
 class Rook(Piece):
      
-     def is_valid_piece_movement(self, starting_square, ending_square):
-        row_difference, column_difference = self.get_row_and_column_difference(starting_square, ending_square)
+    def is_valid_piece_movement(self, row_difference, column_difference):
+        if (row_difference == 0 and column_difference != 0):
+            return True
+        if (row_difference != 0 and column_difference == 0):
+            return True
+        return False
 
 
 class Knight(Piece):
@@ -84,7 +63,13 @@ class Bishop(Piece):
     pass
 
 class Queen(Piece):
-    pass
+    def is_valid_piece_movement(self, row_difference, column_difference):
+        if (row_difference == 0 and column_difference != 0):
+            return True
+        if (row_difference != 0 and column_difference == 0):
+            return True
+
+    
 
 class King(Piece):
     pass
@@ -92,6 +77,17 @@ class King(Piece):
 
 
 '''       
+    def get_row_and_column_difference(self, starting_row, ending_square):
+        #get table coordinates from chess notation
+        starting_row, starting_col = convert_chess_square_to_grid(starting_square)
+        ending_row, ending_col = convert_chess_square_to_grid(ending_square)
+
+        #find difference
+        row_difference = ending_row - starting_row
+        column_difference = ending_col - starting_col
+
+        return row_difference, column_difference        
+
         if self.color == "black":
             if column_difference == 0:
                 if row_difference == 1:
