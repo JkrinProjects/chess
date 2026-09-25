3487b382486b9134cf8e4e76c7c544ce0212462a
jk393 <jkcodingprojects@proton.me>
2026-09-25 19:18:17 -0400

    added attribute for turn and method to change it

--- board.py
+++ board.py
@@ -5,6 +5,7 @@ BOARD_LENGTH = 8
 class Board:
 
     def __init__(self):
+        self.current_turn_color = "w"
         self.grid =  [
         [Rook("black"),Knight("black"),Bishop("black"),Queen("black"),King("black"),Bishop("black"),Knight("black"),Rook("black")],
         [Pawn("black"),Pawn("black"),Pawn("black"),Pawn("black"),Pawn("black"),Pawn("black"),Pawn("black"),Pawn("black")],
@@ -22,6 +23,12 @@ class Board:
         for row in range(BOARD_LENGTH):
             for column in range(BOARD_LENGTH):
                 pass
+
+    def change_turn(self):
+        if self.current_turn_color == "white":
+            self.current_turn_color = "black"
+        else:
+            self.current_turn_color = "white"
 
 
 
